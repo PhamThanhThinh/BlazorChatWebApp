b6914e070b1de88884273f4af5b9ece14360e574
thinh <thinhqn999@gmail.com>
2025-01-11 21:20:46 +0700

    Add project files.

---
+++ BlazorChatWebApp/BlazorChatWebApp/Data/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorChatWebApp.Data.Entities
+{
+  public class User
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime AddOn { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+  }
+}

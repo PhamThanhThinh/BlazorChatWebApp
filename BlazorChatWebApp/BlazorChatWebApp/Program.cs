c0c32989e191c097eb7d582f9b309424b87007d3
thinh <thinhqn999@gmail.com>
2025-01-12 21:14:20 +0700

    hoàn thành token service

--- BlazorChatWebApp/BlazorChatWebApp/Program.cs
+++ BlazorChatWebApp/BlazorChatWebApp/Program.cs
@@ -18,16 +18,8 @@ builder.Services.AddAuthentication(options =>
 }).AddJwtBearer(
   options =>
   {
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-      ValidateIssuer = true,
-      ValidateAudience = true,
-      ValidateLifetime = true,
-      ValidateIssuerSigningKey = true,
-      ValidIssuer = builder.Configuration["Jwt:Issuer"],
-      ValidAudience = builder.Configuration["Jwt:Audience"],
-      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
+    options.TokenValidationParameters = TokenService
+    .GetTokenValidationParameters(builder.Configuration);
   }
   );
 
--- BlazorChatWebApp/BlazorChatWebApp/TokenService.cs
+++ BlazorChatWebApp/BlazorChatWebApp/TokenService.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using BlazorChatWebApp.Data.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace BlazorChatWebApp
 {
@@ -11,24 +14,68 @@ namespace BlazorChatWebApp
       _configuration = configuration;
     }
     
-    public static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
-    {
-      return new TokenValidationParameters
+    public static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration) =>
+    
+      new TokenValidationParameters
       {
         ValidateIssuer = true,
         ValidateAudience = false,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
         ValidIssuer = configuration["Jwt:Issuer"],
-        //ValidAudience = configuration["Jwt:Audience"],
-        //IssuerSigningKey = 
+        IssuerSigningKey = GetSecurityKey(configuration)
+        //ValidAudience = _configuration["Jwt:Audience"],
       };
+
+
+    public string GenerateJWT(IEnumerable<Claim> addClaim = null)
+    {
+      var securityKey = GetSecurityKey(_configuration);
+      var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+      //var expires = DateTime.Now.AddMinutes(30);
+      var expiresInMinutes = Convert.ToInt32(_configuration["Jwt:ExpireInMinutes"] ?? "60");
+
+      var claims = new List<Claim>
+      {
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+      };
+
+      //if (addClaim != null)
+      //{
+      //  claims.AddRange(addClaim);
+      //}
+
+      if (addClaim?.Any() == true)
+      {
+        claims.AddRange(addClaim);
+      }
+
+      var token = new JwtSecurityToken(
+        issuer: _configuration["Jwt:Issuer"],
+        audience: "*",
+        claims: claims,
+        expires: DateTime.Now.AddMinutes(expiresInMinutes),
+        signingCredentials: credentials
+      );
+
+      return new JwtSecurityTokenHandler().WriteToken(token);
     }
 
-    //public string GenerateJwt(IEnumerable<Claim> addClaim = null)
-    //{
-    //  var securityKey = GetSecurityKey();
-    //}
+    public string GenerateJWT(User user, IEnumerable<Claim>? addClaim = null)
+    {
+      var claims = new List<Claim>
+      {
+        new Claim(ClaimTypes.Name, user.Name),
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+      };
+      if (addClaim?.Any() == true)
+      {
+        claims.AddRange(addClaim);
+      }
+      return GenerateJWT(claims);
+    }
 
+    private static SymmetricSecurityKey GetSecurityKey(IConfiguration _configuration) =>
+            new(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
   }
 }
